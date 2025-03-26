34954af1ed139f71e9b0a166a3119caffa829ff1
Amanda <amandagurns@hotmail.ca>
2025-03-26 12:52:44 -0400

    updates

--- src/app/login/authentication.service.ts
+++ src/app/login/authentication.service.ts
@@ -1,10 +1,33 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Apollo, gql, QueryRef } from "apollo-angular";
 
+export interface User {
+  username: string;
+  email: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+@Injectable()
 export class AuthenticationService {
   private userId: string = "";
+  private loginQuery: QueryRef<User>;
 
-  constructor() {
-
+  constructor(private apollo: Apollo) {
+    this.loginQuery = this.apollo.watchQuery({
+      query: gql`
+        query Login {
+          login(username: username, password: password) {
+            username
+            email
+            password
+            created_at
+            updated_at
+          }
+        }
+      `
+    })
   }
 
   saveUserData(id: string, token: string) {
